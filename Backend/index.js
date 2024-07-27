2686c4f13ce42e2aff6bd5e37d2ba2285c16a8ce
unknown <joshuacabrera999@gmail.com>
2024-07-26 22:07:14 -0500

    Arreglado para que se ejecute con la carpeta build (static)

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 const { MONGODB_URI, PORT } = require('./utils/config');
 const usersRouter = require('./controller/controllerUser');
 const propertiesRouter = require('./controller/controllerProperty');
@@ -17,14 +18,22 @@ mongoose.connect(MONGODB_URI)
   console.error('Error connecting to MongoDB:', error.message);
 });
 
-// Middleware para parsear JSON y usar el controlador de usuarios
+// Middleware para parsear JSON y usar los controladores
 app.use(express.json());
-app.use(usersRouter);
-app.use(propertiesRouter);
-app.use(quoteRouter);
-app.use(proformaRouter);
-app.use(reportRouter);
-app.use(supportRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/properties', propertiesRouter);
+app.use('/api/quotes', quoteRouter);
+app.use('/api/proformas', proformaRouter);
+app.use('/api/reports', reportRouter);
+app.use('/api/support', supportRouter);
+
+// Servir archivos estáticos desde la carpeta 'build'
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Manejar cualquier solicitud que no coincida con las rutas definidas anteriormente
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
